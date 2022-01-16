6b50ca560afce25403fbd9f0f7963c0881689ee7
Matthew Britton <mattbritton@hotmail.com>
2022-01-17 09:01:54 +1030

    Fixed the camera switching views so rapidly when launched from an explosion.

--- logic/follow_camera.gd
+++ logic/follow_camera.gd
@@ -10,6 +10,7 @@ export var height = 1.5
 var FOLLOW_SPEED = 2.0  # set 0-1: 0.1=slow follow 0.9=fast follow
 var lerp_val = 0.5
 var target
+var timer_0_5s = 0.5
 
 func _ready():
 	# environment = get_node("/root/TownScene/Viewport/WorldEnvironment")
@@ -33,22 +34,38 @@ func _ready():
 
 
 func _physics_process(delta):
+	
+	timer_0_5s -= delta
+		
 	#var target = get_parent().get_global_transform().origin  # parent is CameraBase pos behind and above the vehicle
 	var target_forward = get_parent().get_node("CamTargetForward").get_global_transform()
 	var target_reverse = get_parent().get_node("CamTargetReverse").get_global_transform()
 	var cam_base_forward = get_parent().get_node("CamBaseForward").get_global_transform()
+	var cam_base_forward2 = get_parent().get_node("CamBaseForward2").get_global_transform()
 	var cam_base_reverse = get_parent().get_node("CamBaseReverse").get_global_transform()
 	
 	var linear_velocity = get_parent().get_parent().linear_velocity
 	var fwd_mps = get_parent().get_parent().transform.basis.xform_inv(linear_velocity).z
+	var angular_velocity = get_parent().get_parent().angular_velocity
+	
+	
+	# when launched by explosion, angular velocity is high and fwd_mps swaps from + to - and is also high
+	# this stops the fast switching in this case - zooms out and stops rotating the camera so fast
+	var follow_speed_multiplier = 1.0 + 0.01*angular_velocity.length()*abs(fwd_mps)
 	
 	if fwd_mps >= -2.0:
-		global_transform = global_transform.interpolate_with(cam_base_forward, delta * FOLLOW_SPEED)
-		target = target.interpolate_with(target_forward, delta * FOLLOW_SPEED)
+		global_transform = global_transform.interpolate_with(cam_base_forward, delta * FOLLOW_SPEED / follow_speed_multiplier)
+		target = target.interpolate_with(target_forward, delta * FOLLOW_SPEED * follow_speed_multiplier)
 	else:
-		global_transform = global_transform.interpolate_with(cam_base_reverse, delta * FOLLOW_SPEED)
-		target = target.interpolate_with(target_reverse, delta * FOLLOW_SPEED)
+		global_transform = global_transform.interpolate_with(cam_base_reverse, delta * FOLLOW_SPEED / follow_speed_multiplier)
+		target = target.interpolate_with(target_reverse, delta * FOLLOW_SPEED * follow_speed_multiplier)
 	
+	if timer_0_5s < 0:
+		# print("angular_velocity.length() = "+str(get_parent().get_parent().angular_velocity.length()))
+		# print("linear_velocity = "+str(linear_velocity))
+		# print("fwd_mps = "+str(fwd_mps))
+		timer_0_5s = 0.5
+		
 	look_at(target.origin, Vector3.UP)
 	
 	# Turn a little up or down

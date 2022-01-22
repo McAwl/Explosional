d47e81c17b241f3dfe57d523be6177fa50f14ecf
Matthew Britton <mattbritton@hotmail.com>
2022-01-23 00:07:00 +1030

    Fixed explosions sending vehicles under the terrain. Tweaked the shadows to look better.

--- logic/missile.gd
+++ logic/missile.gd
@@ -87,21 +87,28 @@ func _on_Missile_body_entered(body):
 		$ExplosionSound.playing = true
 		$ParticlesExplosion.emitting = true
 		hit_something = true
-		velocity = Vector3(0,0,0)
+		#  velocity = Vector3(0,0,0)  # ??????
 			
 		if "CarBody" in body.name:
 			print("Missile hit "+str(body.name))
 			body.hit_by_missile["active"] = true
 			body.hit_by_missile["origin"] = transform.origin
-			body.hit_by_missile["velocity"] = velocity
+			body.hit_by_missile["direction_for_explosion"] = velocity
 			body.hit_by_missile["homing"] = homing
-		else:  # TODO: mvoe this to physics method
+			body.hit_by_missile["direct_hit"] = true
+		else: 
 			for player in get_node("/root/TownScene").get_players():  # in range(1, 5): # explosion toward all players
 				var target = player.get_carbody()  # get_node("/root/TownScene/InstancePos"+str(i)+"/VC/V/CarBase/Body")
 				#var target = get_node("/root/TownScene/InstancePos"+str(i)+"/VC/V/CarBase/Body")
-				var distance = global_transform.origin.distance_to(target.global_transform.origin)
 				var direction = global_transform.origin - target.global_transform.origin
+				var distance = global_transform.origin.distance_to(target.global_transform.origin)
 				if distance < explosion_range:
-					target.apply_impulse( Vector3(0,0,0), 200*direction.normalized() )   # offset, impulse(=direction*force)
-					target.angular_velocity =  Vector3(rng.randf_range(-10, 10), rng.randf_range(-10, 10), rng.randf_range(-10, 10)) 
-					target.damage(1)
+					print("Missile hit "+str(body.name))
+					target.hit_by_missile["active"] = true
+					target.hit_by_missile["origin"] = transform.origin
+					target.hit_by_missile["direction_for_explosion"] = direction
+					target.hit_by_missile["homing"] = homing
+					target.hit_by_missile["direct_hit"] = false
+					target.hit_by_missile["distance"] = distance
+				
+					

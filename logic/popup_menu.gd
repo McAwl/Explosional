26d41d2ee5e5107a4e618b04929d5c3eb893ee13
Matthew Britton <mattbritton@hotmail.com>
2022-05-27 09:28:50 +0930

    Removed terrain editor

--- logic/popup_menu.gd
+++ logic/popup_menu.gd
@@ -22,7 +22,7 @@ func _process(delta):
 	
 func _input(event):
 	if event is InputEventKey and event.pressed:
-		print("event.scancode="+str(event.scancode))
+		# print("event.scancode="+str(event.scancode))
 		if event.scancode != KEY_MASK_META and event.scancode != KEY_META and event.scancode != KEY_SUPER_L and event.scancode != KEY_SUPER_R:  # e.g. Windows key
 			get_tree().paused = false
 
